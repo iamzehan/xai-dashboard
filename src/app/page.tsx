58e5c7eefef2c9b35c7198b29d782638a093986f
Zehan <zehan.kareem@gmail.com>
2026-07-06 01:45:16 +0600

    Add NavBar component to Home page for improved navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { NavBar } from "../components/layout/Navbar";
 import Hero from "../components/sections/Hero";
 
 export default function Home() {
   return (
     <main className="">
+      <NavBar/>
       <Hero />
     </main>
   );
