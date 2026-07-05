c21555d7ac9139d92915e669bd53f16969bf9d76
Zehan <zehan.kareem@gmail.com>
2026-07-06 01:45:49 +0600

    Add NavBar component with responsive navigation and integrated mobile support

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as React from "react";
+import { ArrowRight } from "lucide-react";
+
+import { cn } from "@/src/lib/cn";
+import { Container } from "@/src/components/common/Container";
+import { Button } from "@/src/components/common/Button";
+import { DesktopMenu } from "./DesktopNav";
+import { MobileMenu } from "./MobileMenu";
+import { useIsMobile } from "@/src/hooks";
+import { Logo } from "../ui/Logo";
+export type NavBarProps = React.HTMLAttributes<HTMLElement>;
+
+export const NavBar = React.forwardRef<HTMLElement, NavBarProps>(
+  ({ className, ...props }, ref) => {
+    const isMobile = useIsMobile();
+    return (
+      <header
+        ref={ref}
+        className={cn("fixed inset-x-0 top-0 z-50 py-5", className)}
+      >
+        <Container className="p-0! h-auto!">
+          <nav className="flex items-center justify-between border-b border-(--color-border) bg-surface/70 px-6 py-4 backdrop-blur-xl">
+            <a href="#hero" className="text-xl font-bold tracking-tight">
+              <Logo/>
+              <p className="text-muted font-thin text-xs!">Intelligence within</p>
+            </a>
+
+            <DesktopMenu />
+            {!isMobile && (
+              <Button variant="primary" size="sm" className={`md:inline-flex`}>
+                Get Access
+                <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            )}
+            {isMobile && <MobileMenu />}
+          </nav>
+        </Container>
+      </header>
+    );
+  },
+);
+
+NavBar.displayName = "NavBar";
