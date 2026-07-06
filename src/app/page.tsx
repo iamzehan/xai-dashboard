a66d51deba32dd91087b57d1fd33bd044a226521
Zehan <zehan.kareem@gmail.com>
2026-07-06 15:36:03 +0600

    Refactor Home component layout to include Hero and Features sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { NavBar } from "../components/layout/Navbar";
+import Features from "../components/sections/Features";
 import Hero from "../components/sections/Hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center">
       <NavBar/>
+      {/* Hero Container */}
       <Hero />
+      {/* Feature Cards Container */}
+      <Features/>
     </main>
   );
 }
